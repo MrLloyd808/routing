902ed03c7043c44c55dc694fd6e2cd2fa3e67c5d
LIL-MANIAC-001 <127343644+MrLloyd808@users.noreply.github.com>
2025-05-28 11:13:32 +0200

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useNavigate } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+ 
+  return (
+      <h2>SAINT LLOY</h2>
+  )
+}
+
+export default App
